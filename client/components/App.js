e67cb12d48cf85ed90dfdd07e6e7c1927c30ae04
Loren Mariquit <l.mariquit@gmail.com>
2019-04-17 21:50:21 -0400

    first commit

---
+++ client/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id="main">
+      {/* PUT COMPONENTS HERE */}
+      LM Boilerplate
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public'
+  }
+}
